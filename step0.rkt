4affa0d2bf1dfe502a9b2796aec81bd177d2c630
Rosny Miba Nouguep <rosny.mibanouguep@student.uclouvain.be>
2024-04-06 09:25:11 +0200

    first commit

---
+++ step0.rkt
@@ -0,0 +1,16 @@
+#lang r5rs
+
+; Students: Rosny Miba
+
+(define (point x y)
+  (define (dispatch m)
+    (cond ((eq? m 'getx) x)
+          ((eq? m 'gety) y)
+          ((eq? m 'type) 'point)
+          ((eq? m 'info) (list 'point x y))
+          ((eq? m 'info2) (list (dispatch 'type) (dispatch 'getx) (dispatch 'gety)))))
+  dispatch)
+
+; Yes, we can implement the 'info message in terms of
+; a call to the other messages 'getx, 'gety and 'type
+; look (p 'info2)
---
+++ step1.rkt
@@ -0,0 +1,20 @@
+#lang r5rs
+
+; Students: Rosny Miba
+
+(define (point x y)
+  (define (getx) x)
+  (define (gety) y)
+  (define (type) 'point)
+  (define (info) (list (type) (getx) (gety)))
+  (define (add p) (point (+ x ((p 'getx))) (+ y ((p 'gety)))))
+  (define (setx! a) (set! x a))
+  (define (self m)
+    (cond ((eq? m 'getx) getx)
+          ((eq? m 'gety) gety)
+          ((eq? m 'type) type)
+          ((eq? m 'info) info)
+          ((eq? m 'add) add)
+          ((eq? m 'setx!) setx!)
+          (else (lambda () (display "Message not understood")))))
+  self)
